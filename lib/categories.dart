e597c5d3a7d6ac2a21c6464c7129faf200e1f4d5
Uday Tiwari <udaytiwaricyef2001@gmail.com>
2024-10-06 19:04:00 +0530

    changes

--- lib/categories.dart
+++ lib/categories.dart
@@ -16,7 +16,7 @@ class CategoriesScreen extends StatelessWidget {
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
+        children: const [ 
           Text('1',style: TextStyle(color: Colors.white)),
           Text('2',style: TextStyle(color: Colors.white)),
           Text('3',style: TextStyle(color: Colors.white)),
