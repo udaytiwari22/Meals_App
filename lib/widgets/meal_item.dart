7f1462a7c3b62d6e3db5f8373c2a8122e104204a
Uday Tiwari <udaytiwaricyef2001@gmail.com>
2024-10-09 17:50:41 +0530

    StackWidgets

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({
+    super.key,
+    required this.meal,
+  });
+
+  final Meal meal;
+
+  @override
+  Widget build(context) {
+    return Card(
+      child: InkWell(
+        onTap: () {},
+        child: Stack(
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                color: Colors.black54,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
