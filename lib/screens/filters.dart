e93758f0e619968fa2a662f6ab8fd27f1588110a
Uday Tiwari <udaytiwaricyef2001@gmail.com>
2024-11-18 13:06:25 +0530

    Updated

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -27,12 +27,10 @@ class FilterScreen extends ConsumerWidget {
                   .read(filtersNotifier.notifier)
                   .setFilter(Filter.glutenFree, isChecked);
             },
-            title: Text(
-              'Gluten-free',
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onSurface,
-                  ),
-            ),
+            title: Text('Gluten-free',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    )),
             subtitle: Text(
               'Only Include gluten-free meals.',
               style: Theme.of(context).textTheme.labelMedium!.copyWith(
